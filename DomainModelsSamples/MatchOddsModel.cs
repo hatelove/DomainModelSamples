898667caddf0bdda1bf35c453c2a920613f0713a
Joey Chen <jay.hatelove@gmail.com>
2022-09-21 23:41:02 +0800

    add comments

--- DomainModelsSamples/MatchOddsModel.cs
+++ DomainModelsSamples/MatchOddsModel.cs
@@ -9,13 +9,16 @@ public class MatchOddsModel
     // if replace MatchId property to MatchFull, it can reduce 1 parameter: matchFull
     public IEnumerable<DbOdds> FilterOdds(MatchFull matchFull, int minutes, BetTypesModel betTypesModel)
     {
+        //filter major BetTypes and remove null BetTypes
         var result = OddsList.Where(x => x.BetType.HasValue)
                              .IntersectBy(betTypesModel.MajorBetTypes, odds => odds.BetType!.Value);
 
+        //filter Ht BetTypes 
         result = matchFull.IsFirstHalf() && !IsOverTime(minutes)
             ? RemoveBetTypesOddsWhenNoOddsValue(result, betTypesModel.HtBetTypes)
             : result.ExceptBy(betTypesModel.HtBetTypes, odds => odds.BetType!.Value);
 
+        //filter Ft BetTypes
         return RemoveBetTypesOddsWhenNoOddsValue(result, betTypesModel.FtBetTypes);
     }
 
