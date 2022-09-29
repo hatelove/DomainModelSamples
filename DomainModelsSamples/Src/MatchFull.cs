bef95e7687c1c5e6922333850710ecc421f1c554
Joey Chen <jay.hatelove@gmail.com>
2022-09-29 17:01:38 +0800

    test: add failed test no_scores_when_first_half

--- DomainModelsSamples/Src/MatchFull.cs
+++ DomainModelsSamples/Src/MatchFull.cs
@@ -3,9 +3,21 @@ namespace DomainModelsSamples;
 public class MatchFull
 {
     public int LivePeriod { get; set; }
+    public string GoalRecord { get; set; }
 
     public bool IsFirstHalf()
     {
         return LivePeriod == 1;
     }
+
+    public GoalRecord GetGoalRecord()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class GoalRecord
+{
+    public string FirstHalf { get; set; }
+    public string SecondHalf { get; set; }
 }
---
+++ DomainModelsSamples/Test/GoalRecordTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DomainModelsSamples;
+
+[TestFixture]
+public class GoalRecordTests
+{
+    [Test]
+    public void no_scores_when_first_half()
+    {
+        var matchFull = new MatchFull()
+                        {
+                            LivePeriod = (int)LivePeriodEnum.FirstHalf,
+                            GoalRecord = ""
+                        };
+
+        var goalRecord = matchFull.GetGoalRecord();
+        goalRecord.FirstHalf.Should().Be("");
+        goalRecord.SecondHalf.Should().Be("");
+    }
+}
+
+public enum LivePeriodEnum
+{
+    FirstHalf = 1,
+}
