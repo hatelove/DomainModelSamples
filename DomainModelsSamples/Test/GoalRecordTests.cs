6e1b2d4e4fde23cea520ddf5b80c8705e709f42f
Joey Chen <jay.hatelove@gmail.com>
2022-09-29 17:16:22 +0800

    test: add pass test only_second_half_has_scores_when_full_half

--- DomainModelsSamples/Test/GoalRecordTests.cs
+++ DomainModelsSamples/Test/GoalRecordTests.cs
@@ -49,6 +49,16 @@ public class GoalRecordTests
         goalRecord.SecondHalf.Should().Be("");
     }
 
+    [Test]
+    public void only_second_half_has_scores_when_full_half()
+    {
+        var matchFull = GivenMatch(LivePeriodEnum.FullHalf, ";AA");
+
+        var goalRecord = matchFull.GetGoalRecord();
+        goalRecord.FirstHalf.Should().Be("");
+        goalRecord.SecondHalf.Should().Be("AA");
+    }
+
     private static MatchFull GivenMatch(LivePeriodEnum livePeriod, string goalRecord)
     {
         return new MatchFull()
