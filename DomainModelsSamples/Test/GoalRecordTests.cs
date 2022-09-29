a0bf9da3fe38f4f41aceb70080ee62f04d82d991
Joey Chen <jay.hatelove@gmail.com>
2022-09-29 17:08:20 +0800

    test: add failed test scores_when_first_half

--- DomainModelsSamples/Test/GoalRecordTests.cs
+++ DomainModelsSamples/Test/GoalRecordTests.cs
@@ -12,14 +12,24 @@ public class GoalRecordTests
     [Test]
     public void no_scores_when_first_half()
     {
-        var matchFull = GivnMatch(LivePeriodEnum.FirstHalf, "");
+        var matchFull = GivenMatch(LivePeriodEnum.FirstHalf, "");
 
         var goalRecord = matchFull.GetGoalRecord();
         goalRecord.FirstHalf.Should().Be("");
         goalRecord.SecondHalf.Should().Be("");
     }
 
-    private static MatchFull GivnMatch(LivePeriodEnum livePeriod, string goalRecord)
+    [Test]
+    public void scores_when_first_half()
+    {
+        var matchFull = GivenMatch(LivePeriodEnum.FirstHalf, "HA");
+
+        var goalRecord = matchFull.GetGoalRecord();
+        goalRecord.FirstHalf.Should().Be("HA");
+        goalRecord.SecondHalf.Should().Be("");
+    }
+
+    private static MatchFull GivenMatch(LivePeriodEnum livePeriod, string goalRecord)
     {
         return new MatchFull()
                {
